50cd80fbbf3a45c6820935a9bdcf865dacc246d1
goberomsu <gobeumsu@gmail.com>
2023-07-31 21:58:29 +0900

    Feat : OrderRestController Test

---
+++ src/test/java/com/example/kakao/order/OrderRestControllerTest.java
@@ -0,0 +1,72 @@
+package com.example.kakao.order;
+
+import com.example.kakao.MyRestDoc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ActiveProfiles("test")
+@Sql("classpath:db/teardown.sql")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class OrderRestControllerTest extends MyRestDoc {
+
+  @Autowired
+  private ObjectMapper om;
+
+  @WithUserDetails(value = "ssarmango@nate.com")
+  @Test
+  public void save_test() throws Exception {
+
+    ResultActions resultActions = mvc.perform(
+        post("/carts/orders/save")
+    );
+    String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+    System.out.println("Save Test: " + responseBody);
+
+    resultActions.andExpect(jsonPath("$.success").value("true"));
+    resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+  }
+
+  @WithUserDetails(value = "ssarmango@nate.com")
+  @Test
+  public void findById_test() throws Exception {
+    int id = 1;
+    ResultActions resultActions = mvc.perform(
+        get("/carts/orders/" + id)
+    );
+
+    String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+    System.out.println("FindById Test: " + responseBody);
+
+    resultActions.andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.response.id").value(1))
+        .andExpect(jsonPath("$.response.products[0].productName").value(
+            "기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"))
+        .andExpect(jsonPath("$.response.products[0].items[0].id").value(1))
+        .andExpect(jsonPath("$.response.products[0].items[0].quantity").value(5))
+        .andExpect(jsonPath("$.response.products[0].items[0].price").value(50000))
+        .andExpect(jsonPath("$.response.products[0].items[0].optionName").value(
+            "01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+
+    resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+  }
+}
