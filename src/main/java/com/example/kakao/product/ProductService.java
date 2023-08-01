77ab646f4286dc5209ed9cb130efdae42f4e8ed8
goberomsu <gobeumsu@gmail.com>
2023-08-01 10:56:25 +0900

    Feat : FindAll 예외처리

--- src/main/java/com/example/kakao/product/ProductService.java
+++ src/main/java/com/example/kakao/product/ProductService.java
@@ -18,41 +18,36 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class ProductService {
+
     private final ProductJPARepository productRepository;
     private final OptionJPARepository optionRepository;
 
     public ProductResponse.FindByIdDTOv2 findByIdv2(int id) {
         List<Option> optionListPS = optionRepository.findByProductIdJoinProduct(id);
-        if(optionListPS.isEmpty()){
-            throw new Exception404("해당 상품을 찾을 수 없습니다 : "+id);
+        if (optionListPS.isEmpty()) {
+            throw new Exception404("해당 상품을 찾을 수 없습니다 : " + id);
         }
         return new ProductResponse.FindByIdDTOv2(optionListPS);
     }
 
     public ProductResponse.FindByIdDTO findById(int id) {
         Product productPS = productRepository.findById(id).orElseThrow(
-                () -> new Exception404("해당 상품을 찾을 수 없습니다 : "+id)
+                () -> new Exception404("해당 상품을 찾을 수 없습니다 : " + id)
         );
         List<Option> optionListPS = optionRepository.findByProductId(productPS.getId());
         return new ProductResponse.FindByIdDTO(productPS, optionListPS);
     }
 
 
-
     public List<ProductResponse.FindAllDTO> findAll(int page) {
-        // 1. 페이지 객체 만들기
-        Pageable pageable = PageRequest.of(page,9);
-
-        // 2. DB 조회하기
+        Pageable pageable = PageRequest.of(page, 9);
         Page<Product> pageContent = productRepository.findAll(pageable);
-
-        // 3. DTO 만들기
-        // 페이지 객체의 content는 List이다.
-        // List를 stream()으로 변환 -> 자바 오브젝트의 타입이 없어진다. (강물에 흩뿌린다)
-        // map으로 순회하면서 값을 변한한다. (가공)
-        // 가공된 데이터를 다시 자바 오브젝트로 변환한다.
+        if (pageContent.isEmpty()) {
+            throw new Exception404("해당 페이지에서 product를 찾지 못하였습니다: " + page);
+        }
         return pageContent.getContent().stream()
                 .map(FindAllDTO::new)
                 .collect(Collectors.toList());
     }
+
 }
