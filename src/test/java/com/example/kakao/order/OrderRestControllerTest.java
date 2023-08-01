0dfea331bbd7d362dca5ba5a59d57d828a66944e
goberomsu <gobeumsu@gmail.com>
2023-08-01 10:56:38 +0900

    Feat : Order Rest Controller

--- src/test/java/com/example/kakao/order/OrderRestControllerTest.java
+++ src/test/java/com/example/kakao/order/OrderRestControllerTest.java
@@ -1,12 +1,17 @@
 package com.example.kakao.order;
 
 import com.example.kakao.MyRestDoc;
+import com.example.kakao._core.errors.exception.Exception400;
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.cart.CartJPARepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,9 +24,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 @Sql("classpath:db/teardown.sql")
@@ -29,44 +37,84 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class OrderRestControllerTest extends MyRestDoc {
 
-  @Autowired
-  private ObjectMapper om;
+    @Autowired
+    private ObjectMapper om;
 
-  @WithUserDetails(value = "ssarmango@nate.com")
-  @Test
-  public void save_test() throws Exception {
+    @MockBean
+    private CartJPARepository cartJPARepository;
 
-    ResultActions resultActions = mvc.perform(
-        post("/carts/orders/save")
-    );
-    String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-    System.out.println("Save Test: " + responseBody);
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void save_test() throws Exception {
 
-    resultActions.andExpect(jsonPath("$.success").value("true"));
-    resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
-  }
+        ResultActions resultActions = mvc.perform(
+                post("/carts/orders/save")
+        );
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("Save Test: " + responseBody);
 
-  @WithUserDetails(value = "ssarmango@nate.com")
-  @Test
-  public void findById_test() throws Exception {
-    int id = 1;
-    ResultActions resultActions = mvc.perform(
-        get("/carts/orders/" + id)
-    );
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
 
-    String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-    System.out.println("FindById Test: " + responseBody);
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findById_test() throws Exception {
+        int id = 1;
+        ResultActions resultActions = mvc.perform(
+                get("/carts/orders/" + id)
+        );
 
-    resultActions.andExpect(jsonPath("$.success").value(true))
-        .andExpect(jsonPath("$.response.id").value(1))
-        .andExpect(jsonPath("$.response.products[0].productName").value(
-            "기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"))
-        .andExpect(jsonPath("$.response.products[0].items[0].id").value(1))
-        .andExpect(jsonPath("$.response.products[0].items[0].quantity").value(5))
-        .andExpect(jsonPath("$.response.products[0].items[0].price").value(50000))
-        .andExpect(jsonPath("$.response.products[0].items[0].optionName").value(
-            "01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("FindById Test: " + responseBody);
 
-    resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
-  }
+        resultActions.andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.response.id").value(1))
+                .andExpect(jsonPath("$.response.products[0].productName").value(
+                        "기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"))
+                .andExpect(jsonPath("$.response.products[0].items[0].id").value(1))
+                .andExpect(jsonPath("$.response.products[0].items[0].quantity").value(5))
+                .andExpect(jsonPath("$.response.products[0].items[0].price").value(50000))
+                .andExpect(jsonPath("$.response.products[0].items[0].optionName").value(
+                        "01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
+
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findById_test_with_invalid_id() throws Exception {
+        int id = -1; // invalid id
+        ResultActions resultActions = mvc.perform(
+                get("/carts/orders/" + id)
+        );
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("findById_test_with_invalid_id : " + responseBody);
+
+        resultActions.andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof Exception404))
+                .andExpect(result -> assertEquals("해당 주문이 존재하지 않습니다 : " + id,
+                        result.getResolvedException().getMessage()));
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void save_test_with_empty_cart() throws Exception {
+        Mockito.when(cartJPARepository.findByUserIdOrderByOptionIdAsc(Mockito.anyInt()))
+                .thenReturn(new ArrayList<>());
+
+        ResultActions resultActions = mvc.perform(
+                post("/carts/orders/save")
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("save_test_with_empty_cart : " + responseBody);
+
+        resultActions.andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof Exception400))
+                .andExpect(
+                        result -> assertEquals("장바구니가 비어있습니다.", result.getResolvedException().getMessage()));
+    }
+
+
 }
