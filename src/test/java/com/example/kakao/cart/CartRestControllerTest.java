773860c3b9b940f1d458225241132079b219db5c
goberomsu <gobeumsu@gmail.com>
2023-08-02 13:19:14 +0900

    Feat : cart 관련 기능 테스트 구현

--- src/test/java/com/example/kakao/cart/CartRestControllerTest.java
+++ src/test/java/com/example/kakao/cart/CartRestControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -36,7 +35,7 @@ public class CartRestControllerTest extends MyRestDoc {
         // given -> optionId [1,2,16]이 teardown.sql을 통해 들어가 있음
         List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
         CartRequest.SaveDTO item = new CartRequest.SaveDTO();
-        item.setOptionId(3);
+        item.setOptionId(2);
         item.setQuantity(5);
         requestDTOs.add(item);
 
@@ -57,33 +56,6 @@ public class CartRestControllerTest extends MyRestDoc {
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 
-    @WithUserDetails(value = "ssarmango@nate.com")
-    @Test
-    public void findAll_test() throws Exception {
-        // given teardown
-
-        // when
-        ResultActions resultActions = mvc.perform(
-                get("/carts")
-        );
-
-        // eye
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : " + responseBody);
-
-        // verify
-        resultActions.andExpect(jsonPath("$.success").value("true"));
-        resultActions.andExpect(jsonPath("$.response.products[0].id").value(1));
-        resultActions.andExpect(jsonPath("$.response.products[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].id").value(1));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.id").value(1));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.optionName").value("01. 슬라이딩 지퍼백 크리스마스에디션 4종"));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].option.price").value(10000));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].quantity").value(5));
-        resultActions.andExpect(jsonPath("$.response.products[0].carts[0].price").value(50000));
-        resultActions.andExpect(jsonPath("$.response.totalPrice").value(310900));
-        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
-    }
 
     @WithUserDetails(value = "ssarmango@nate.com")
     @Test
@@ -117,5 +89,38 @@ public class CartRestControllerTest extends MyRestDoc {
         resultActions.andExpect(jsonPath("$.response.carts[0].price").value(100000));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void addCartList_test_no_exception() throws Exception {
+        // Given
+        List<CartRequest.SaveDTO> requestDTOs = new ArrayList<>();
+        CartRequest.SaveDTO item = new CartRequest.SaveDTO();
+        int optionId = 100;  // Non-existent option ID to trigger Exception400
+
+        item.setOptionId(optionId);  // Non-existent option ID to trigger Exception400
+        item.setQuantity(5);
+        requestDTOs.add(item);
+
+        String requestBody = om.writeValueAsString(requestDTOs);
+
+        // When
+        ResultActions resultActions = mvc.perform(
+                post("/carts/add")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        // Then expect Exception400
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        resultActions.andExpect(jsonPath("$.success").value("false"));
+        resultActions.andExpect(jsonPath("$.response").isEmpty());
+        resultActions.andExpect(jsonPath("$.error.status").value("404"));
+        resultActions.andExpect(jsonPath("$.error.message").value("Could not find that option: " + optionId));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
 
 }
